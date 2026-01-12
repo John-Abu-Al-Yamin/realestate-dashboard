f12b4bf1b4849d0b6ab9540ea6ea61899839646c
Your Name <john@arabgiftcard.com>
2026-01-12 19:28:08 +0200

    add

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  base: "/realestate/",
+  base: "/",
   plugins: [
     tailwindcss(),
 
@@ -18,8 +18,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  // build: {
+  //   outDir: "dist",
+  //   emptyOutDir: true,
+  // },
 });
